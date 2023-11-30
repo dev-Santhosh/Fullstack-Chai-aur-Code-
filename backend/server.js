4625671043cbf955516944956ef408c4fba9379b
Santhosh <spinisetti6@gmail.com>
2023-11-30 13:20:51 +0530

    FullStack Deployed

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+// const express = require('express');
+import express from 'express';
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Server is ready.');
+// });
+
+app.get("/api/jokes", (req,res)=>{
+  const jokes = [
+    {
+      id:1,
+      title:"A joke",
+      content:"First joke",
+    },
+    {
+      id:2,
+      title:"Another joke",
+      content:"Second joke",
+    },
+    {
+      id:3,
+      title:"A third joke",
+      content:"Third joke",
+    },
+    {
+      id:4,
+      title:"A fourth joke",
+      content:"Fourth joke",
+    },
+    {
+      id:5,
+      title:"A fifth joke",
+      content:"Fifth joke",
+    },
+  ];
+  res.send(jokes);
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
